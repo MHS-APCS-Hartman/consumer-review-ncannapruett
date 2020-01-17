14183fdb783444f23f5ab0c68256049eeae488e8
Anna Pruett <59542027+ncannapruett@users.noreply.github.com>
2020-01-17 10:09:17 -0500

    Update Review.java
    I added the starRating method to my Review.java. Could you review it and make sure it looks okay? @mvsine @huntermargaux

--- Review.java
+++ Review.java
@@ -188,4 +188,25 @@ public class Review {
    }
    return totalVal; 
 }
+  public static int starRating(String fileName) 
+{
+   int totalSentiment = (int) totalSentiment(fileName);
+   
+   if(totalSentiment < 0) 
+   {
+      return 1; 
+   }
+   else if(totalSentiment < 5) 
+   {
+      return 2; 
+   }
+   else if(totalSentiment < 15) 
+   {
+      return 3;
+   }
+   else 
+   {
+      return 4; 
+   }
+}
 }
